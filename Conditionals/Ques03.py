5850414d6b49a80e14dab1b01e9cfdc591638b60
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-04 19:35:13 +0530

    Question 3 added

---
+++ Conditionals/Ques03.py
@@ -0,0 +1,8 @@
+#  3. Accept an integer and check whether it is an even number or odd.
+
+a = int(input("Enter an integer: "))
+
+if a%2 == 0 :
+    print("Even number")
+else:
+    print("Odd number")
