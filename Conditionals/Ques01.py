4495ea57e93b8fddacb7a02204250665320ff77a
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-04 17:30:33 +0530

    Question 1 added

---
+++ Conditionals/Ques01.py
@@ -0,0 +1,12 @@
+# 1. Accept two numbers and print the greater between them.
+
+a = input("Enter first number: ")
+
+b = input("Enter second number: ")
+
+if a > b:
+    print(f"{a} is the bigger")
+elif a == b:
+    print("Both are equal")
+else :
+    print(f"{b} is the bigger")
