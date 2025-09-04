dbbd93c2f49fc77fc5a99327a900a46947e6db6e
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-04 19:36:37 +0530

    Question 4 added

---
+++ Conditionals/Ques04.py
@@ -0,0 +1,8 @@
+# Q4. Accept name and age from the user. Check if the user is a valid voter or not.
+
+a = int(input("Enter your age : "))
+
+if a <= 18 :
+       print("You are not eligible")
+else :
+       print("You can vote")
