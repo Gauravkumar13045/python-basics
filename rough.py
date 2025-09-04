999d76fc968ff7ea78fc5b18c93ecbc0cf43e877
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-04 17:24:21 +0530

    python file created

---
+++ rough.py
@@ -0,0 +1,77 @@
+# print("Gaurav Kumar")
+
+# a = "Gaurav Kumar"
+
+# print(a[0::1])
+
+# input("Press any key to exit")
+
+# 0 
+
+# print(5**1000)
+
+# a = 10
+ 
+# b = print(a + 20)
+
+# c = print( b + 40)
+
+# d = print (c + 60)
+
+# print(a, b, c, d)
+
+
+# print(126 > 130)
+# print((456 == 456) != (235 == 236))
+# print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 13)
+# print (True and bool(0))
+
+# fee = int(input("Enter amount of fee:"))
+
+# if fee == 5000: 
+#       print("You have to pay more")
+
+# else:
+#       print("you don't have to pay")      
+
+
+# a = input("Enter first number: ")
+
+# b = input("Enter second number: ")
+
+# if a > b:
+#     print(f"{a} is the bigger")
+ 
+# else :
+#     print(f"{b} is the bigger")
+
+
+# a = input("Enter your gender :")
+
+# if a == "male":
+#      print("Good Morning Sir")
+# else :
+#      print("Good Morning Mam")     
+
+# a = int(input("Enter the number : "))
+
+# if a <= 18 :
+#        print("You are not eligible")
+# else :
+#        print("You can vote")
+
+# a = int(input("Enter the year"))
+
+# if a%4 == 0 :
+#     print(f"{a} is a leap year")
+# else :
+#     print(f"{a} is not a leap year")    
+    
+
+
+a = input("Enter the temperature :")
+
+if a < 0 :
+        print("Freezing cold")
+elif a         
+
