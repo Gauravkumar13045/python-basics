7519649a5ef31672cda1d90a26bca2d246a39962
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-06 18:37:54 +0530

    Question 12 added

--- loops/Ques11.py
+++ loops/Ques11.py
@@ -4,5 +4,4 @@ a = int(input("Enter the number to get the sum: "))
 b = 0
 for i in range(1, a + 1, 1):
     b = b + i
-
 print("The final sum is:", b)
---
+++ loops/Ques12.py
@@ -0,0 +1,13 @@
+# 12. Factorial of a number
+
+a = int(input("Enter the number to get the factorial : "))
+b = 1
+
+if a < 0:
+    print("Factorial does not exist for negative numbers.")
+elif a == 0:
+    print("The factorial of 0 is 1.")
+else:
+    for i in range(1, a + 1):
+        b = b * i
+    print("The factorial of", a, "is", b)
