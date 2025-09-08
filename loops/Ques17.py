f5b1aa37344eab4b8fed8699bffbf75d6a5a648a
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-08 14:38:06 +0530

    Question 17 added

--- loops/Ques17.py
+++ loops/Ques17.py
@@ -1,14 +1,12 @@
 # Reverse a string without using in build functions.
 
-# Accept input from user
-original = input("Enter a string to reverse: ")
 
-# Initialize an empty string to hold the reversed result
+original = input("Enter a string to reverse: ")
 reversed_string = ""
 
-# Loop through the original string in reverse order
+
 for i in range(len(original) - 1, -1, -1):
     reversed_string += original[i]
 
-# Print the reversed string
+
 print("Reversed string:", reversed_string)
---
+++ loops/Ques18.py
@@ -0,0 +1,19 @@
+# Count all letters, digits, and special symbols from a given string 
+a = input("Enter a string: ")
+
+letters = 0
+number = 0
+symbol = 0
+
+for ch in  a:
+    ascii_val = ord(ch)
+    if(65 <= ascii_val <= 90) or (97 <= ascii_val <= 122):
+        letters += 1
+    elif 48 <= ascii_val <= 57:
+        number += 1
+    else:
+        symbol += 1
+
+print("Letters:", letters)
+print("Digits:", number)
+print("Symbols:", symbol)
