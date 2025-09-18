39e09d46c2f1cc2095c8233fc23ff64dbe4b313c
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-18 14:40:33 +0530

    Ques27 added

---
+++ List/Ques27.py
@@ -0,0 +1,18 @@
+# Check if List is sorted or not.
+
+print()
+print("To know the greatest number please input your numbers:")
+print()
+
+a = int(input("Enter the number a: "))
+b = int(input("Enter the number b: "))
+c = int(input("Enter the number c: "))
+d = int(input("Enter the number d: "))
+
+lister = [a, b, c, d]
+
+# Use sorted() to compare with original list
+if lister == sorted(lister):
+    print("Sorted")
+else:
+    print("Unsorted")
