ba9da880ea1bb5b8d7af2f0ea02f244679aa0cd3
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-09-18 22:22:34 +0530

    Ques28 added

---
+++ Dictionary/Ques28.py
@@ -0,0 +1,10 @@
+# Write a Python script to merge two Python dictionaries
+
+student = {"name": "Gaurav", "age": 20 }
+teacher = {"name": "krishna", "age": 50}
+
+merged = student | teacher
+
+print("Merged dictionary: ", merged)
+
+
